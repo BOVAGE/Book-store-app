e5ddfe8bbbc62765cb719a93949235a6d5674b55
FAYEMI BOLUWATIFE <67077455+BOVAGE@users.noreply.github.com>
2020-11-14 00:38:59 -1200

    refactored codes

--- backend.py
+++ backend.py
@@ -1,6 +1,7 @@
 import sqlite3
 
 def connect():
+    """ Establish connection to database file """
     conn = sqlite3.connect("books.db")
     cur = conn.cursor()
     cur.execute("CREATE TABLE IF NOT EXISTS book(id INTEGER PRIMARY KEY, title TEXT, author TEXT, year INTEGER, isbn INTEGER)")
@@ -8,39 +9,50 @@ def connect():
     conn.close()
 
 def insert(title,author,year, isbn):
+    """ adds book record to databse """
     conn = sqlite3.connect("books.db")
     cur = conn.cursor()
     cur.execute("INSERT INTO book VALUES(NULL,?,?,?,?)",(title,author,year, isbn))
     conn.commit()
     conn.close()
 
-def view():
+def view(widget):
+    """ displays the books in the database in the listbox widget  """
     conn = sqlite3.connect("books.db")
     cur = conn.cursor()
     cur.execute("SELECT * FROM book")
     rows = cur.fetchall()
     conn.close()
-    return rows
+    widget.delete(0, "end")
+    for book in rows:
+        widget.insert("end", book)
 
-def update(id, title, author, year, isbn):
+def update(id, widget, title, author, year, isbn):
+    """ update the selected row """
     conn = sqlite3.connect("books.db")
     cur = conn.cursor()
     cur.execute("UPDATE book SET title = ?, author = ?, year = ?, isbn = ? WHERE id = ?",(title, author, year, isbn, id))
     conn.commit()
     conn.close()
+    view(widget)
 
-def delete(id):
+def delete(id, widget):
+    """ deletes selected row """
     conn = sqlite3.connect("books.db")
     cur = conn.cursor()
     cur.execute("DELETE FROM book where id = ?", (id,))
     conn.commit()
     conn.close()
+    view(widget)
 
-def search(title = "", author = "", year ="", isbn = ""):
+def search(widget, title = "", author = "", year ="", isbn = ""):
+    """ search and display query """
     conn = sqlite3.connect("books.db")
     cur = conn.cursor()
     cur.execute("SELECT * from book WHERE title = ? or author = ? or year = ? or isbn = ?",(title, author, year, isbn))
     rows = cur.fetchall()
     conn.commit()
     conn.close()
-    return rows
+    widget.delete(0, "end")
+    for book in rows:
+        widget.insert("end", book)
--- frontend.py
+++ frontend.py
@@ -9,44 +9,17 @@ root.configure(bg = "powderblue")
 def select():
     """ returns the selected row of listbox widget  """
     return textBox.get(textBox.curselection())
-    
-def viewCommand():
-    """ displays the books in the database in the listbox widget  """
-    listOfBooks = bd.view()
-    textBox.delete(0, "end")
-    for book in listOfBooks:
-        textBox.insert("end", book)
 
 def close():
     """ closes the window """
     root.destroy()
-
-def delete():
-    """ deletes selected row """ 
-    id = select()[0]
-    bd.delete(id)
-    viewCommand()
     
-def search(title = "", author = "", year ="", isbn = ""):
-    """ search and display query """
-    listOfBooks = bd.search(title, author, year, isbn)
-    print(listOfBooks)
-    textBox.delete(0, "end")
-    for book in listOfBooks:
-        textBox.insert("end", book)
-        print("Search")
-
 def show(*event):
     """ shows the selected row in their entries"""
     titleInput.set(select()[1])
     yearInput.set(select()[2])
     authorInput.set(select()[3])
     isbnInput.set(select()[4])
-
-def update():
-    """ update the selected row """
-    bd.update(select()[0],titleInput.get(),authorEntry.get(),yearEntry.get(), isbnEntry.get())
-    viewCommand()
 
 title = tk.Label(root, text = "Title", font = ("Arial", 13))
 title.grid(row = 0, column = 0)
@@ -86,22 +59,23 @@ scrollBar.grid(row = 3, column = 2, rowspan = 2)
 textBox.configure(yscrollcommand = scrollBar.set)
 scrollBar.configure(command = textBox.yview)
 
-view = tk.Button(root, text = "View All", width = 13, bd = 3, command = viewCommand)
+view = tk.Button(root, text = "View All", width = 13, bd = 3, command = lambda: bd.view(textBox))
 view.grid(row = 2,column = 3, pady = 0)
 
 searchEntry = tk.Button(root, text = "Search Entry", width = 13, bd = 3,
-                        command = lambda: search(titleInput.get(),authorEntry.get(),yearEntry.get(), isbnEntry.get()))
+                        command = lambda: bd.search(textBox,titleInput.get(),authorEntry.get(),yearEntry.get(), isbnEntry.get()))
 searchEntry.grid(row = 3,column = 3, pady = 0)
 
 addEntry = tk.Button(root, text = "Add Entry", width = 13, bd = 3,
                      command = lambda:bd.insert(titleInput.get(),authorEntry.get(),yearEntry.get(), isbnEntry.get()))
 addEntry.grid(row = 4,column = 3, pady = 0)
 
-updateSelected = tk.Button(root, text = "Update Selected", width = 13, bd = 3, command = update)
+updateSelected = tk.Button(root, text = "Update Selected", width = 13, bd = 3,
+                           command = lambda: bd.update(select()[0], textBox, titleInput.get(),authorEntry.get(),yearEntry.get(), isbnEntry.get()))
 updateSelected.grid(row = 5,column = 3, pady = 0)
 
 deleteSelected = tk.Button(root, text = "Delete Selected", width = 13, bd = 3,
-                           command = delete)
+                           command = lambda: bd.delete(select()[0], textBox))
 deleteSelected.grid(row = 6,column = 3, pady = 0)
 
 close = tk.Button(root, text = "Close", bg = "Red", width = 13, bd = 3, command = close)
